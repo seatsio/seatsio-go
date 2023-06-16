57a7099e5d0c297ffb38899200f331884c8cf8c5
Ben Verbeken <ben@seats.io>
2023-06-16 21:22:50 +0200

    deep event summary reports

--- reports/event_reports.go
+++ reports/event_reports.go
@@ -14,11 +14,59 @@ type EventDeepSummaryReport struct {
 }
 
 type EventDeepSummaryReportItem struct {
-	Count int `json:"count,omitempty"`
+	Count           int                               `json:"count,omitempty"`
+	ByStatus        map[string]EventSummaryReportItem `json:"byStatus,omitempty"`
+	ByCategoryKey   map[string]EventSummaryReportItem `json:"byCategoryKey,omitempty"`
+	ByCategoryLabel map[string]EventSummaryReportItem `json:"byCategoryLabel,omitempty"`
+	BySection       map[string]EventSummaryReportItem `json:"bySection,omitempty"`
+	ByAvailability  map[string]EventSummaryReportItem `json:"byAvailability,omitempty"`
+	ByChannel       map[string]EventSummaryReportItem `json:"byChannel,omitempty"`
+}
+
+type EventSummaryReportItem struct {
+	Count                int            `json:"count,omitempty"`
+	ByStatus             map[string]int `json:"byStatus,omitempty"`
+	ByCategoryKey        map[string]int `json:"byCategoryKey,omitempty"`
+	ByCategoryLabel      map[string]int `json:"byCategoryLabel,omitempty"`
+	BySection            map[string]int `json:"bySection,omitempty"`
+	ByAvailability       map[string]int `json:"byAvailability,omitempty"`
+	ByAvailabilityReason map[string]int `json:"byAvailabilityReason,omitempty"`
+	ByChannel            map[string]int `json:"byChannel,omitempty"`
 }
 
 func (reports *EventReports) DeepSummaryByStatus(eventKey string) (*EventDeepSummaryReport, error) {
-	reportType := "byStatus"
+	return reports.fetchEventDeepSummaryReport("byStatus", eventKey)
+}
+
+func (reports *EventReports) DeepSummaryByObjectType(eventKey string) (*EventDeepSummaryReport, error) {
+	return reports.fetchEventDeepSummaryReport("byObjectType", eventKey)
+}
+
+func (reports *EventReports) DeepSummaryByCategoryKey(eventKey string) (*EventDeepSummaryReport, error) {
+	return reports.fetchEventDeepSummaryReport("byCategoryKey", eventKey)
+}
+
+func (reports *EventReports) DeepSummaryByCategoryLabel(eventKey string) (*EventDeepSummaryReport, error) {
+	return reports.fetchEventDeepSummaryReport("byCategoryLabel", eventKey)
+}
+
+func (reports *EventReports) DeepSummaryBySection(eventKey string) (*EventDeepSummaryReport, error) {
+	return reports.fetchEventDeepSummaryReport("bySection", eventKey)
+}
+
+func (reports *EventReports) DeepSummaryByAvailability(eventKey string) (*EventDeepSummaryReport, error) {
+	return reports.fetchEventDeepSummaryReport("byAvailability", eventKey)
+}
+
+func (reports *EventReports) DeepSummaryByAvailabilityReason(eventKey string) (*EventDeepSummaryReport, error) {
+	return reports.fetchEventDeepSummaryReport("byAvailabilityReason", eventKey)
+}
+
+func (reports *EventReports) DeepSummaryByChannel(eventKey string) (*EventDeepSummaryReport, error) {
+	return reports.fetchEventDeepSummaryReport("byChannel", eventKey)
+}
+
+func (reports *EventReports) fetchEventDeepSummaryReport(reportType string, eventKey string) (*EventDeepSummaryReport, error) {
 	var report map[string]EventDeepSummaryReportItem
 	result, err := reports.Client.R().
 		SetSuccessResult(&report).
--- reports_test/event_report_deep-summary_test.go
+++ reports_test/event_report_deep-summary_test.go
@@ -22,7 +22,136 @@ func TestDeepSummaryByStatus(t *testing.T) {
 
 	report, err := client.EventReports.DeepSummaryByStatus(event.Key)
 	require.NoError(t, err)
-	require.Equal(t, 1, report.Items[events.ObjectStatusBooked].Count)
-	// require.Equal(t, 1, report.Items[events.ObjectStatusBooked].Count) TODO bver
+	require.Equal(t, 1, report.Items["booked"].Count)
+	require.Equal(t, 1, report.Items["booked"].BySection["NO_SECTION"].Count)
+	require.Equal(t, 1, report.Items["booked"].BySection["NO_SECTION"].ByAvailability["not_available"])
 
+}
+
+func TestDeepSummaryByObjectType(t *testing.T) {
+	t.Parallel()
+	company := test_util.CreateTestCompany(t)
+	client := seatsio.NewSeatsioClient(company.Admin.SecretKey, test_util.BaseUrl)
+	chartKey := test_util.CreateTestChart(t, company.Admin.SecretKey)
+	event, _ := client.Events.Create(&events.CreateEventParams{ChartKey: chartKey})
+
+	report, err := client.EventReports.DeepSummaryByObjectType(event.Key)
+	require.NoError(t, err)
+	require.Equal(t, 32, report.Items["seat"].Count)
+	require.Equal(t, 32, report.Items["seat"].BySection["NO_SECTION"].Count)
+	require.Equal(t, 32, report.Items["seat"].BySection["NO_SECTION"].ByAvailability["available"])
+}
+
+func TestDeepSummaryByCategoryKey(t *testing.T) {
+	t.Parallel()
+	company := test_util.CreateTestCompany(t)
+	client := seatsio.NewSeatsioClient(company.Admin.SecretKey, test_util.BaseUrl)
+	chartKey := test_util.CreateTestChart(t, company.Admin.SecretKey)
+	event, _ := client.Events.Create(&events.CreateEventParams{ChartKey: chartKey})
+	client.Events.ChangeObjectStatus(&events.StatusChangeParams{
+		Status:  events.ObjectStatusBooked,
+		Events:  []string{event.Key},
+		Objects: []events.ObjectProperties{{ObjectId: "A-1"}},
+	})
+
+	report, err := client.EventReports.DeepSummaryByCategoryKey(event.Key)
+	require.NoError(t, err)
+	require.Equal(t, 116, report.Items["9"].Count)
+	require.Equal(t, 116, report.Items["9"].BySection["NO_SECTION"].Count)
+	require.Equal(t, 1, report.Items["9"].BySection["NO_SECTION"].ByAvailability["not_available"])
+}
+
+func TestDeepSummaryByCategoryLabel(t *testing.T) {
+	t.Parallel()
+	company := test_util.CreateTestCompany(t)
+	client := seatsio.NewSeatsioClient(company.Admin.SecretKey, test_util.BaseUrl)
+	chartKey := test_util.CreateTestChart(t, company.Admin.SecretKey)
+	event, _ := client.Events.Create(&events.CreateEventParams{ChartKey: chartKey})
+	client.Events.ChangeObjectStatus(&events.StatusChangeParams{
+		Status:  events.ObjectStatusBooked,
+		Events:  []string{event.Key},
+		Objects: []events.ObjectProperties{{ObjectId: "A-1"}},
+	})
+
+	report, err := client.EventReports.DeepSummaryByCategoryLabel(event.Key)
+	require.NoError(t, err)
+	require.Equal(t, 116, report.Items["Cat1"].Count)
+	require.Equal(t, 116, report.Items["Cat1"].BySection["NO_SECTION"].Count)
+	require.Equal(t, 1, report.Items["Cat1"].BySection["NO_SECTION"].ByAvailability["not_available"])
+}
+
+func TestDeepSummaryBySection(t *testing.T) {
+	t.Parallel()
+	company := test_util.CreateTestCompany(t)
+	client := seatsio.NewSeatsioClient(company.Admin.SecretKey, test_util.BaseUrl)
+	chartKey := test_util.CreateTestChart(t, company.Admin.SecretKey)
+	event, _ := client.Events.Create(&events.CreateEventParams{ChartKey: chartKey})
+	client.Events.ChangeObjectStatus(&events.StatusChangeParams{
+		Status:  events.ObjectStatusBooked,
+		Events:  []string{event.Key},
+		Objects: []events.ObjectProperties{{ObjectId: "A-1"}},
+	})
+
+	report, err := client.EventReports.DeepSummaryBySection(event.Key)
+	require.NoError(t, err)
+	require.Equal(t, 232, report.Items["NO_SECTION"].Count)
+	require.Equal(t, 116, report.Items["NO_SECTION"].ByCategoryLabel["Cat1"].Count)
+	require.Equal(t, 1, report.Items["NO_SECTION"].ByCategoryLabel["Cat1"].ByAvailability["not_available"])
+}
+
+func TestDeepSummaryByAvailability(t *testing.T) {
+	t.Parallel()
+	company := test_util.CreateTestCompany(t)
+	client := seatsio.NewSeatsioClient(company.Admin.SecretKey, test_util.BaseUrl)
+	chartKey := test_util.CreateTestChart(t, company.Admin.SecretKey)
+	event, _ := client.Events.Create(&events.CreateEventParams{ChartKey: chartKey})
+	client.Events.ChangeObjectStatus(&events.StatusChangeParams{
+		Status:  events.ObjectStatusBooked,
+		Events:  []string{event.Key},
+		Objects: []events.ObjectProperties{{ObjectId: "A-1"}},
+	})
+
+	report, err := client.EventReports.DeepSummaryByAvailability(event.Key)
+	require.NoError(t, err)
+	require.Equal(t, 1, report.Items["not_available"].Count)
+	require.Equal(t, 1, report.Items["not_available"].ByCategoryLabel["Cat1"].Count)
+	require.Equal(t, 1, report.Items["not_available"].ByCategoryLabel["Cat1"].BySection["NO_SECTION"])
+}
+
+func TestDeepSummaryByAvailabilityReason(t *testing.T) {
+	t.Parallel()
+	company := test_util.CreateTestCompany(t)
+	client := seatsio.NewSeatsioClient(company.Admin.SecretKey, test_util.BaseUrl)
+	chartKey := test_util.CreateTestChart(t, company.Admin.SecretKey)
+	event, _ := client.Events.Create(&events.CreateEventParams{ChartKey: chartKey})
+	client.Events.ChangeObjectStatus(&events.StatusChangeParams{
+		Status:  events.ObjectStatusBooked,
+		Events:  []string{event.Key},
+		Objects: []events.ObjectProperties{{ObjectId: "A-1"}},
+	})
+
+	report, err := client.EventReports.DeepSummaryByAvailabilityReason(event.Key)
+	require.NoError(t, err)
+	require.Equal(t, 1, report.Items["booked"].Count)
+	require.Equal(t, 1, report.Items["booked"].ByCategoryLabel["Cat1"].Count)
+	require.Equal(t, 1, report.Items["booked"].ByCategoryLabel["Cat1"].BySection["NO_SECTION"])
+}
+
+func TestDeepSummaryByChannel(t *testing.T) {
+	t.Parallel()
+	company := test_util.CreateTestCompany(t)
+	client := seatsio.NewSeatsioClient(company.Admin.SecretKey, test_util.BaseUrl)
+	chartKey := test_util.CreateTestChart(t, company.Admin.SecretKey)
+	event, _ := client.Events.Create(&events.CreateEventParams{ChartKey: chartKey})
+	client.Events.ChangeObjectStatus(&events.StatusChangeParams{
+		Status:  events.ObjectStatusBooked,
+		Events:  []string{event.Key},
+		Objects: []events.ObjectProperties{{ObjectId: "A-1"}},
+	})
+
+	report, err := client.EventReports.DeepSummaryByChannel(event.Key)
+	require.NoError(t, err)
+	require.Equal(t, 232, report.Items["NO_CHANNEL"].Count)
+	require.Equal(t, 116, report.Items["NO_CHANNEL"].ByCategoryLabel["Cat1"].Count)
+	require.Equal(t, 116, report.Items["NO_CHANNEL"].ByCategoryLabel["Cat1"].BySection["NO_SECTION"])
 }
