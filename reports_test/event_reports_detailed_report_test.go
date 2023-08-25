0eecca9556184b788ec6d3104159e52692c7a4ac
Matti Roloux <matti@seats.io>
2023-08-25 14:16:09 +0200

    Fixed test

--- reports_test/event_reports_detailed_report_test.go
+++ reports_test/event_reports_detailed_report_test.go
@@ -17,7 +17,7 @@ func TestDetailedReportItemProperties(t *testing.T) {
 	chartKey := test_util.CreateTestChart(t, company.Admin.SecretKey)
 	event, err := client.Events.Create(&events.CreateEventParams{ChartKey: chartKey, EventParams: &events.EventParams{
 		Channels: &[]events.CreateChannelParams{
-			{Key: "channelKey1", Name: "channel 1", Color: "#FFFF99", Index: 1, Objects: []string{"A-1"}},
+			{Key: "channel1", Name: "channel 1", Color: "#FFFF99", Index: 1, Objects: []string{"A-1"}},
 		},
 	}})
 	require.NoError(t, err)
