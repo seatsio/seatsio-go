ce897dd95a1dd38577e2dbf773206ab643a42a46
Steve Chaloner <steve@seats.io>
2023-10-16 14:08:15 +0200

    Fixes race condition in test

--- holdtokens_test/update_hold_token_expiration_date_test.go
+++ holdtokens_test/update_hold_token_expiration_date_test.go
@@ -24,5 +24,5 @@ func TestUpdateHoldTokenExpirationDate(t *testing.T) {
 	require.True(t, updatedHoldToken.ExpiresAt.After(start.Add(-29*time.Minute)))
 	require.True(t, updatedHoldToken.ExpiresAt.Before(start.Add(31*time.Minute)))
 	require.True(t, updatedHoldToken.ExpiresInSeconds > 29*60)
-	require.True(t, updatedHoldToken.ExpiresInSeconds < 30*60)
+	require.True(t, updatedHoldToken.ExpiresInSeconds <= 30*60)
 }
