0a669060cb2d4070cf11ead8a6c168ae5aea3035
Steve Chaloner <steve@seats.io>
2023-10-18 15:53:58 +0200

    Fixed race condition in hold token tests

--- holdtokens_test/retrieve_hold_token_test.go
+++ holdtokens_test/retrieve_hold_token_test.go
@@ -18,5 +18,8 @@ func TestRetrieveHoldToken(t *testing.T) {
 	retrievedHoldToken, err := client.HoldTokens.Retrieve(holdToken.HoldToken)
 	require.NoError(t, err)
 
-	require.Equal(t, holdToken, retrievedHoldToken)
+	require.NotEmpty(t, retrievedHoldToken.HoldToken)
+	require.Equal(t, holdToken.ExpiresAt, retrievedHoldToken.ExpiresAt)
+	require.True(t, retrievedHoldToken.ExpiresInSeconds > 14*60)
+	require.True(t, retrievedHoldToken.ExpiresInSeconds <= 15*60)
 }
