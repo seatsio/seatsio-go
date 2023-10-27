9f56d7667134abebdbff1d61c6fe982ca37ddeb7
Matti Roloux <matti@seats.io>
2023-10-27 12:03:44 +0200

    Added call to override the season object status

---
+++ events_test/override_season_object_status_test.go
@@ -0,0 +1,28 @@
+package events
+
+import (
+	"github.com/seatsio/seatsio-go/v6"
+	"github.com/seatsio/seatsio-go/v6/events"
+	"github.com/seatsio/seatsio-go/v6/seasons"
+	"github.com/seatsio/seatsio-go/v6/test_util"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestOverrideSeasonObjectStatus(t *testing.T) {
+	t.Parallel()
+	company := test_util.CreateTestCompany(t)
+	chartKey := test_util.CreateTestChart(t, company.Admin.SecretKey)
+	client := seatsio.NewSeatsioClient(test_util.BaseUrl, company.Admin.SecretKey)
+	season, err := client.Seasons.CreateSeasonWithOptions(chartKey, &seasons.CreateSeasonParams{EventKeys: []string{"event1"}})
+	require.NoError(t, err)
+	_, err = client.Events.Book(season.Key, "A-1", "A-2")
+	require.NoError(t, err)
+
+	err = client.Events.OverrideSeasonObjectStatus("event1", "A-1", "A-2")
+	require.NoError(t, err)
+
+	info, _ := client.Events.RetrieveObjectInfo("event1", "A-1", "A-2")
+	require.Equal(t, events.FREE, info["A-1"].Status)
+	require.Equal(t, events.FREE, info["A-2"].Status)
+}
