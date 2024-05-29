23bb7ced302ccd958bfbdc7182898528fcc54fae
mroloux <matti@seats.io>
2024-05-29 11:11:38 +0200

    Exposed venue type (#51)

--- charts/chart.go
+++ charts/chart.go
@@ -3,16 +3,17 @@ package charts
 import "github.com/seatsio/seatsio-go/v7/events"
 
 type Chart struct {
-	Id                           int64                 `json:"id"`
-	Key                          string                `json:"key"`
-	Name                         string                `json:"name"`
-	Status                       string                `json:"status"`
-	Tags                         []string              `json:"tags"`
-	PublishedVersionThumbnailUrl string                `json:"publishedVersionThumbnailUrl"`
-	DraftVersionThumbnailUrl     string                `json:"draftVersionThumbnailUrl"`
-	Events                       []events.Event        `json:"events"`
-	Archived                     bool                  `json:"archived"`
-	Validation                   ChartValidationResult `json:"validation"`
+	Id                           int64                  `json:"id"`
+	Key                          string                 `json:"key"`
+	Name                         string                 `json:"name"`
+	Status                       string                 `json:"status"`
+	Tags                         []string               `json:"tags"`
+	PublishedVersionThumbnailUrl string                 `json:"publishedVersionThumbnailUrl"`
+	DraftVersionThumbnailUrl     string                 `json:"draftVersionThumbnailUrl"`
+	Events                       []events.Event         `json:"events"`
+	Archived                     bool                   `json:"archived"`
+	Validation                   *ChartValidationResult `json:"validation"`
+	VenueType                    string                 `json:"venueType"`
 }
 
 type Tags struct {
